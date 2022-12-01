53945f205503cec5f5bd5819e4eae3c098cbd713
Guillaume Mantopoulos <3082385+MrGuiMan@users.noreply.github.com>
2022-12-01 18:14:25 +0100

    day1 cleanup

--- src/day1.rs
+++ src/day1.rs
@@ -4,13 +4,12 @@ use std::io::{BufRead, BufReader};
 pub fn print_answer() {
     let file = File::open("assets/day1_puzzle1_input.txt").unwrap();
     let f = BufReader::new(file);
-
-    let mut max_calories: Vec<i32> = vec![0, 2, 4];
+    let mut max_calories: Vec<i32> = vec![0, 0, 0];
     let mut calories_count: i32 = 0;
 
     for line_result in f.lines() {
         let line: String = line_result.unwrap();
-        if line.len() > 0 {
+        if !line.is_empty() {
             calories_count += line.parse::<i32>().unwrap();
         } else {
             let insert_index = max_calories.iter().position(|&x| x <= calories_count);
@@ -28,9 +27,6 @@ pub fn print_answer() {
     println!(
         "Max Calories: {:?}, {}",
         max_calories.clone(),
-        max_calories
-            .into_iter()
-            .reduce(|acc, curr| acc + curr)
-            .unwrap()
+        max_calories.iter().sum::<i32>()
     );
 }
