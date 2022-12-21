09a1ccb54650c4c9881ffe47f79e6b666724a9c8
Guillaume Mantopoulos <3082385+MrGuiMan@users.noreply.github.com>
2022-12-21 09:15:20 +0100

    complete day 12

--- src/day12.rs
+++ src/day12.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use std::collections::HashSet;
 
 #[derive(Clone, Debug, Hash, Eq, PartialEq)]
@@ -17,24 +18,10 @@ impl Node {
             index: y * (row_length) + x,
         }
     }
+    fn get_char(&self, input: &str) -> char {
+        input.chars().nth(self.index).unwrap()
+    }
 }
-
-fn get_target_nodes(input: &str) -> (Node, Node) {
-    let row_length = input.find('\n').unwrap() + 1;
-    input
-        .chars()
-        .enumerate()
-        .filter(|(_, char)| *char == 'S' || *char == 'E')
-        .fold(
-            (Node::new(0, row_length), Node::new(0, row_length)),
-            |acc, (i, char)| match char {
-                'S' => (Node::new(i, row_length), acc.1.clone()),
-                'E' => (acc.0.clone(), Node::new(i, row_length)),
-                _ => acc,
-            },
-        )
-}
-
 fn translate_char(char: Option<char>) -> Option<char> {
     match char {
         Some('S') => Some('a'),
@@ -42,8 +29,11 @@ fn translate_char(char: Option<char>) -> Option<char> {
         x => x,
     }
 }
-
-fn get_accessible_neighboors(input: &str, node: &Node) -> Vec<Node> {
+fn get_accessible_neighboors(
+    input: &str,
+    node: &Node,
+    filter: fn(&str, usize, Vec<Node>) -> Vec<Node>,
+) -> Vec<Node> {
     let mut neighboors: Vec<Node> = vec![];
     let last_visible_char_index = input.find('\n').unwrap() - 1;
     let row_length = last_visible_char_index + 2;
@@ -61,23 +51,54 @@ fn get_accessible_neighboors(input: &str, node: &Node) -> Vec<Node> {
         neighboors.push(Node::new(node.index + row_length, row_length));
     }
 
+    filter(input, node.index, neighboors)
+}
+fn filter_part1_node_neighboors(
+    input: &str,
+    current_char_index: usize,
+    neighboors: Vec<Node>,
+) -> Vec<Node> {
     neighboors
         .into_iter()
         .filter(|target_node| {
             let diff = (translate_char(input.chars().nth(target_node.index)).unwrap() as u8) as i32
-                - (translate_char(input.chars().nth(node.index)).unwrap() as u8) as i32;
+                - (translate_char(input.chars().nth(current_char_index)).unwrap() as u8) as i32;
             diff <= 1
         })
         .collect::<Vec<Node>>()
 }
 
+fn filter_part2_node_neighboors(
+    input: &str,
+    current_char_index: usize,
+    neighboors: Vec<Node>,
+) -> Vec<Node> {
+    neighboors
+        .into_iter()
+        .filter(|target_node| {
+            let diff = (translate_char(input.chars().nth(target_node.index)).unwrap() as u8) as i32
+                - (translate_char(input.chars().nth(current_char_index)).unwrap() as u8) as i32;
+            diff >= -1
+        })
+        .collect::<Vec<Node>>()
+}
+
 pub fn get_part1_answer(input: &str) -> String {
-    let (start, end) = get_target_nodes(input); // could probably do without crawling the entire input to find start and end beforehand
-    let mut nodes_to_explore: Vec<(Node, u32)> = Vec::from([(start.clone(), 0)]);
+    let row_length = input.find('\n').unwrap() + 1;
+    let start_node = Node::new(
+        input
+            .chars()
+            .enumerate()
+            .find(|(_, char)| *char == 'S')
+            .unwrap()
+            .0,
+        row_length,
+    );
+    let mut nodes_to_explore: Vec<(Node, u32)> = Vec::from([(start_node.clone(), 0)]);
     let mut explored_nodes: HashSet<Node> = HashSet::new();
 
     while let Some((node, distance)) = nodes_to_explore.pop() {
-        if node == end {
+        if node.get_char(input) == 'E' {
             return distance.to_string();
         }
         if explored_nodes.contains(&node) {
@@ -85,7 +106,8 @@ pub fn get_part1_answer(input: &str) -> String {
         }
         explored_nodes.insert(node.clone());
 
-        let accessible_neighboors = get_accessible_neighboors(input, &node);
+        let accessible_neighboors =
+            get_accessible_neighboors(input, &node, filter_part1_node_neighboors);
         nodes_to_explore.append(
             &mut accessible_neighboors
                 .into_iter()
@@ -97,8 +119,45 @@ pub fn get_part1_answer(input: &str) -> String {
         nodes_to_explore.sort_by(|(_, a), (_, b)| b.cmp(a));
     }
 
-    String::from("Couldn't find the end, sorry")
+    String::from("couldn't find the end, sorry")
 }
-pub fn get_part2_answer(_input: &str) -> String {
-    String::new()
+pub fn get_part2_answer(input: &str) -> String {
+    let row_length = input.find('\n').unwrap() + 1;
+    let start = Node::new(
+        input
+            .chars()
+            .enumerate()
+            .find(|(_, char)| *char == 'E')
+            .unwrap()
+            .0,
+        row_length,
+    );
+
+    let mut nodes_to_explore: Vec<(Node, u32)> = Vec::from([(start.clone(), 0)]);
+    let mut explored_nodes: HashSet<Node> = HashSet::new();
+    let mut shortest_distance_to_a = u32::MAX;
+
+    while let Some((node, distance)) = nodes_to_explore.pop() {
+        if node.get_char(input) == 'a' {
+            shortest_distance_to_a = cmp::min(shortest_distance_to_a, distance);
+        }
+        if explored_nodes.contains(&node) {
+            continue;
+        }
+        explored_nodes.insert(node.clone());
+
+        let accessible_neighboors =
+            get_accessible_neighboors(input, &node, filter_part2_node_neighboors);
+        nodes_to_explore.append(
+            &mut accessible_neighboors
+                .into_iter()
+                .filter(|node| !explored_nodes.contains(node))
+                .map(|node| (node, distance + 1))
+                .collect::<Vec<(Node, u32)>>(),
+        );
+
+        nodes_to_explore.sort_by(|(_, a), (_, b)| b.cmp(a));
+    }
+
+    shortest_distance_to_a.to_string()
 }
